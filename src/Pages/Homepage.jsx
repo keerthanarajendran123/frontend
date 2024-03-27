56045e2eb41dbfec322ae6c9be60b2f794b0f2b6
keerthanarajendran123 <keerthurb9@gmail.com>
2024-03-27 15:53:07 +0530

    login

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,68 +1,18 @@
-// import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
-// import { useEffect } from "react";
-// import { Navigate } from "react-router-dom"; // Import Navigate
-// import Login from "../components/Authentication/Login";
-// import Signup from "../components/Authentication/Signup";
-
-// function Homepage() {
-//   useEffect(() => {
-//     const user = JSON.parse(localStorage.getItem("userInfo"));
-//     if (user) {
-//       return <Navigate to="/chats" />;
-//     }
-//   }, []);
-
-//   return (
-//     <Container maxW="xl" centerContent>
-//       <Box
-//         d="flex"
-//         justifyContent="center"
-//         p={3}
-//         bg="white"
-//         w="100%"
-//         m="40px 0 15px 0"
-//         borderRadius="lg"
-//         borderWidth="1px"
-//       >
-//         <Text fontSize="4xl" fontFamily="Work sans">
-//           MERN-CHAT-APP
-//         </Text>
-//       </Box>
-//       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
-//         <Tabs isFitted variant="soft-rounded">
-//           <TabList mb="1em">
-//             <Tab>Login</Tab>
-//             <Tab>Sign Up</Tab>
-//           </TabList>
-//           <TabPanels>
-//             <TabPanel>
-//               <Login />
-//             </TabPanel>
-//             <TabPanel>
-//               <Signup />
-//             </TabPanel>
-//           </TabPanels>
-//         </Tabs>
-//       </Box>
-//     </Container>
-//   );
-// }
-
-// export default Homepage;
-
 import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
 import { useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom"; // Import useNavigate
+import { Navigate, useNavigate } from "react-router-dom"; 
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 function Homepage() {
-  const navigate = useNavigate(); // Initialize navigate function
-
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem("userInfo"));
-    if (user) {
-      navigate("/chats"); // Redirect to "/chats" route
-    }
-  }, [navigate]); // Include navigate in the dependency array
+    const navigate = useNavigate(); 
+  
+    useEffect(() => {
+      const user = JSON.parse(localStorage.getItem("userInfo"));
+      if (user) {
+        navigate("/chats"); 
+      }
+    }, [navigate]); 
 
   return (
     <Container maxW="xl" centerContent>
@@ -101,3 +51,57 @@ function Homepage() {
 }
 
 export default Homepage;
+
+// import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
+// import { useEffect } from "react";
+// import { Navigate, useNavigate } from "react-router-dom";
+// import Login from "../components/Authentication/Login";
+// import Signup from "../components/Authentication/Signup 
+
+// function Homepage() {
+//   const navigate = useNavigate(); // Initialize navigate function
+
+//   useEffect(() => {
+//     const user = JSON.parse(localStorage.getItem("userInfo"));
+//     if (user) {
+//       navigate("/chats"); // Redirect to "/chats" route
+//     }
+//   }, [navigate]); // Include navigate in the dependency array
+
+//   return (
+//     <Container maxW="xl" centerContent>
+//       <Box
+//         d="flex"
+//         justifyContent="center"
+//         p={3}
+//         bg="white"
+//         w="100%"
+//         m="40px 0 15px 0"
+//         borderRadius="lg"
+//         borderWidth="1px"
+//       >
+//         <Text fontSize="4xl" fontFamily="Work sans">
+//           MERN-CHAT-APP
+//         </Text>
+//       </Box>
+//       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+//         <Tabs isFitted variant="soft-rounded">
+//           <TabList mb="1em">
+//             <Tab>Login</Tab>
+//             <Tab>Sign Up</Tab>
+//           </TabList>
+//           <TabPanels>
+//             <TabPanel>
+//               <Login />
+//             </TabPanel>
+//             <TabPanel>
+//               <Signup />
+//             </TabPanel>
+//           </TabPanels>
+//         </Tabs>
+//       </Box>
+//     </Container>
+//   );
+// }
+
+// export default Homepage;
