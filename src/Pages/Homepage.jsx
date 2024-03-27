b6da86c7aeda4816be6a9d888050d2c3d194b731
keerthanarajendran123 <keerthurb9@gmail.com>
2024-03-27 15:09:56 +0530

    Homepage updt

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,16 +1,68 @@
+// import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
+// import { useEffect } from "react";
+// import { Navigate } from "react-router-dom"; // Import Navigate
+// import Login from "../components/Authentication/Login";
+// import Signup from "../components/Authentication/Signup";
+
+// function Homepage() {
+//   useEffect(() => {
+//     const user = JSON.parse(localStorage.getItem("userInfo"));
+//     if (user) {
+//       return <Navigate to="/chats" />;
+//     }
+//   }, []);
+
+//   return (
+//     <Container maxW="xl" centerContent>
+//       <Box
+//         d="flex"
+//         justifyContent="center"
+//         p={3}
+//         bg="white"
+//         w="100%"
+//         m="40px 0 15px 0"
+//         borderRadius="lg"
+//         borderWidth="1px"
+//       >
+//         <Text fontSize="4xl" fontFamily="Work sans">
+//           MERN-CHAT-APP
+//         </Text>
+//       </Box>
+//       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+//         <Tabs isFitted variant="soft-rounded">
+//           <TabList mb="1em">
+//             <Tab>Login</Tab>
+//             <Tab>Sign Up</Tab>
+//           </TabList>
+//           <TabPanels>
+//             <TabPanel>
+//               <Login />
+//             </TabPanel>
+//             <TabPanel>
+//               <Signup />
+//             </TabPanel>
+//           </TabPanels>
+//         </Tabs>
+//       </Box>
+//     </Container>
+//   );
+// }
+
+// export default Homepage;
+
 import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
 import { useEffect } from "react";
-import { Navigate } from "react-router-dom"; // Import Navigate
-import Login from "../components/Authentication/Login";
-import Signup from "../components/Authentication/Signup";
+import { Navigate, useNavigate } from "react-router-dom"; // Import useNavigate
 
 function Homepage() {
+  const navigate = useNavigate(); // Initialize navigate function
+
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("userInfo"));
     if (user) {
-      return <Navigate to="/chats" />;
+      navigate("/chats"); // Redirect to "/chats" route
     }
-  }, []);
+  }, [navigate]); // Include navigate in the dependency array
 
   return (
     <Container maxW="xl" centerContent>
@@ -49,4 +101,3 @@ function Homepage() {
 }
 
 export default Homepage;
-
